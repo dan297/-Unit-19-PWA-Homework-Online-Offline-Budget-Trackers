ee68480df86f3cd6c86882987c28d04ccae509e8
Dan <Danielwhite98@icloud.com>
2021-11-01 23:42:00 +0000

    //installing service worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -6,6 +6,16 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
   ];
-  
+
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
+
+//installing service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
