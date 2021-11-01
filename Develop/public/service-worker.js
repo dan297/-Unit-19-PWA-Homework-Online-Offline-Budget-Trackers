8efd874465c70ae399a279afd01ab1ef6717e414
Dan <Danielwhite98@icloud.com>
2021-11-01 23:39:05 +0000

    files to cache

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+  ];
+  
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
