4f14a1866375003951243f70962c517a5f195f57
Dan <Danielwhite98@icloud.com>
2021-11-03 19:06:04 +0000

    update db.js

--- public/db.js
+++ public/db.js
@@ -1,18 +1,77 @@
-
 let db;
-const request = indexedDB.open("budget", 1);
+let budgetVersion;
 
-request.onupgradeneeded = function(event) {
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
 };
 
-request.onsuccess = function(event) {
-  db = event.target.result;
+request.onerror = function (e) {
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+  console.log('check db invoked');
+
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  const store = transaction.objectStore('BudgetStore');
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+            const currentStore = transaction.objectStore('BudgetStore');
+
+            currentStore.clear();
+            console.log('Clearing store 🧹');
+          }
+        });
+    }
+  };
+}
+request.onsuccess = function (e) {
+  console.log('success');
+  db = e.target.result;
 
   if (navigator.onLine) {
+    console.log('Backend online! 🗄️');
     checkDatabase();
   }
 };
 
-window.addEventListener("online", checkDatabase);
+const saveRecord = (record) => {
+  console.log('Save record invoked');
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  const store = transaction.objectStore('BudgetStore');
+
+  store.add(record);
+};
+
+window.addEventListener('online', checkDatabase);
